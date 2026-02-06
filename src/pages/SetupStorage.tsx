81ae0992004963dea9051b143833afcacbfa09a5
thedarkdoctor <thedarkdoctorytoffical@gmail.com>
2026-02-06 09:17:05 -0600

    Update SetupStorage.tsx

--- src/pages/SetupStorage.tsx
+++ src/pages/SetupStorage.tsx
@@ -1,4 +1,4 @@
-"import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '@/integrations/supabase/client';
 import { Button } from '@/components/ui/button';
@@ -207,4 +207,3 @@ export default function SetupStorage() {
     </div>
   );
 }
-"
